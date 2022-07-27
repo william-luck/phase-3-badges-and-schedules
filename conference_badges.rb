a572bb7e012b2494eec03d141d2fff0885dd6230
William Luck <luck.william.h@gmail.com>
2022-07-27 14:53:48 -0500

    commit tree 8c2e627379fd2ce113fdec4b82a23bfdaa56bb9f

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,22 @@
 # Write your code here.
+
+require "pry"
+
+def badge_maker(string)
+    "Hello, my name is #{string}."
+end
+
+def batch_badge_creator(names)
+    names.map {|name| badge_maker(name)}
+end
+
+def assign_rooms(speakers) 
+    speakers.map.with_index {|name, index| "Hello, #{name}! You'll be assigned to room #{index + 1}!" }
+end
+
+def printer(speakers)
+    batch_badge_creator(speakers).each {|badge| puts badge}
+    assign_rooms(speakers).each {|assignment| puts assignment}
+end
+
+binding.pry
